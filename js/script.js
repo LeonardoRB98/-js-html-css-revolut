adc2c04a5c407f8976e0f8d50b125fb888a05ae4
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-11-25 18:21:45 +0100

    parte javascript ok da sistemare position absolute per gli li

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 $(document).ready(
   function() {
-
-    $('#header_right ul li:nth-child(4)').hover(
+    // seleziono tutti gli li con classe toggle
+    var list = $('#header_right ul li.toggle')
+    console.log(list);
+    list.mouseenter(
       function() {
-        $('.dropdown').toggleClass('active');
+        // console.log(alert('funziona'));
+        $(this).children('.dropdown').toggle();
       }
-    );
-
-
-
-
-
-
-
-
-
+    )
+    list.mouseleave(
+      function() {
+        $(this).children('.dropdown').toggle();
+      }
+    )
   }
-)
+);
