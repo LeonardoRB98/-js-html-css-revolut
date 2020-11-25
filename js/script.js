c42bd7386a00a3452bb627761eb37d3a2e9d437b
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-11-25 16:59:59 +0100

    prova js hover

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(
+  function() {
+
+    $('#header_right ul li:nth-child(4)').hover(
+      function() {
+        $('.dropdown').toggleClass('active');
+      }
+    );
+
+
+
+
+
+
+
+
+
+  }
+)
