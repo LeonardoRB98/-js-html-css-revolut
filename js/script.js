1ab590c8447719294def45f6e8f1558bca129621
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-11-26 09:53:24 +0100

    click toggle function

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@ $(document).ready(
     // seleziono tutti gli li con classe toggle
     var list = $('#header_right ul li.toggle')
     console.log(list);
-    
+
     list.mouseenter(
       function() {
         // console.log(alert('funziona'));
         $(this).children('.dropdown').toggle();
       }
-    )
+    );
     list.mouseleave(
       function() {
         $(this).children('.dropdown').toggle();
       }
-    )
+    );
+
+    list.click(
+      function() {
+        $(this).children('.dropdown').toggle();
+      }
+    );
   }
 );
